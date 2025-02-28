35f1926ccf3396d7b96b687895e3a76c700ff9af
Emílio Sarges <emiliosarges@gmail.com>
2025-02-28 15:52:58 -0300

    vue commit 1

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Primeiro Componente</h1>
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-color: green;
+    padding: 20px;
+    margin: 30px;
+    border-radius: 5px;
+    text-align: center;
+    color: white;
+}
+
+div:hover {
+    background-color: greenyellow;
+    color: black;
+}
+
+h1 {
+    margin-bottom: 10px;
+}
+</style>
