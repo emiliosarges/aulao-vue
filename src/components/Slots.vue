07fed0a43be5a16db2a1f9668490b338cf216f3c
Emílio Silva <emiliosarges@gmail.com>
2025-03-02 22:20:19 -0300

    Final Aulão Vue

---
+++ src/components/Slots.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="componente">
+        <h1>Compornente com Slots</h1>
+        <button>
+            <slot name="icone"></slot> 
+            <slot name="texto">Botão</slot>
+        </button>
+    </div>
+</template>
